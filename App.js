dd75142ab85192f0fefae436e7db8675b55ca1b7
Go-Groups-Nasah <you@example.com>
2023-10-21 13:14:14 +0100

    testing

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './src/components/HomePage';
+// import Login from
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <NavigationContainer>
+      <HomeScreen />
+      </NavigationContainer>
     </View>
   );
 }
